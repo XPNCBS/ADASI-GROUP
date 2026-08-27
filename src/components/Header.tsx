851209181d961e4acb157927efc1e9814cc12e30
SerbianGhost <serbianghost816@gmail.com>
2026-08-27 20:19:16 +0500

    fix header scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,22 @@ import Logo from "../assets/logo";
 import initializeHeaderController from "../controllers/headerController";
 import Navbar from "./shared/Navbar";
 import LanguageSwitcher from "./shared/LanguageSwitcher";
-
+import { useState } from "react";
 export default function Header() {
+  const [isScrolled,setScrolled] = useState(false)
   useEffect(() => {
-    return initializeHeaderController();
+    const handleScroll = ()=>{
+      setScrolled(window.scrollY > 0)
+
+    }
+    window.addEventListener("scroll",handleScroll)
+    handleScroll()
+    return()=> window.removeEventListener("scroll",handleScroll)
   }, []);
 
   return (
     <>
-      <header className="header">
+      <header className={`header ${isScrolled ? "scrolled" : ""}`}>
         <div className="header-container">
           <div className="header-logo">
             <div style={{ width: "50px", height: "50px" }}>
