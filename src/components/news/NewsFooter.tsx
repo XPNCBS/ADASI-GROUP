b41ab2b229c509b38e713fd2478313d927063552
XPNCBS <d6412248@gmail.com>
2026-09-04 19:21:49 +0500

    чиним тэйлвайнд

--- src/components/news/NewsFooter.tsx
+++ src/components/news/NewsFooter.tsx
@@ -1,36 +1,26 @@
 import NewsButton from "./shared/NewsButton";
-
+import { HiMiniBuildingOffice } from "react-icons/hi2";
+import { FaPhone } from "react-icons/fa";
+import { MdPlace } from "react-icons/md";
 export default function NewsFooter() {
   return (
     <footer className="news-footer">
       <div className="news-footer-grid">
         <div>
-          <div className="news-subscribe-title">ПОДПИСАТЬСЯ НА НОВОСТИ</div>
-          <div className="news-subscribe-row">
-            <input
-              type="email"
-              placeholder="Введите свой Email"
-              className="news-form-input news-subscribe-input"
-            />
-            <NewsButton className="news-subscribe-button">
-              Подписаться
-            </NewsButton>
-          </div>
-          <div className="news-copyright">
-            АВТОРСКОЕ ПРАВО © 2024 ГРУППА ADASI
-          </div>
+          
+      
         </div>
 
         <div className="news-footer-info">
-          <div>
-            🏢 Компания: ADASI GROUP SDN BHD / Пн - Пт: 10:00 - 18:00 /
+          <div className="news-footer-info-item"> 
+             <span className="font-bold text-gray-600 flex"><HiMiniBuildingOffice/> Адрес:</span> ADASI GROUP SDN BHD / Пн - Пт: 10:00 - 18:00 /
             info@adasigroup.com
           </div>
-          <div>
-            📍 Адрес: N-05-03, Pusat Perdagangan, Bandar Bukit Jalil,
+          <div className="news-footer-info-item">
+            <span className="font-bold text-gray-600 flex"> <FaPhone/> Адрес:</span> N-05-03, Pusat Perdagangan, Bandar Bukit Jalil,
             Куала-Лумпур, Малайзия 57000
           </div>
-          <div>📞 Тел.: (+60) 3 9766 8630 / Факс: (+60) 3 9766 8631</div>
+          <div className="news-footer-info-item"><span className="font-bold text-gray-600 flex"><MdPlace/> Тел.:</span> (+60) 3 9766 8630 / Факс: (+60) 3 9766 8631</div>
         </div>
       </div>
     </footer>
